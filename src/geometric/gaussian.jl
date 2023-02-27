d88d25cd03bb7d0023e24129aa75f871b1141fa0
Kazunori Akiyama <kakiyama@mit.edu>
2023-02-27 12:16:35 -0500

    v0.1.2: add a shortcut for a general two dimensional Gaussian model

--- src/geometric/gaussian.jl
+++ src/geometric/gaussian.jl
@@ -1,4 +1,8 @@
-export Gaussian, GaussianFilter
+export EllipticalGaussian
+export CircularGaussian
+export Gaussian
+export GaussianFilter
+
 
 """
     $(TYPEDEF)
@@ -38,4 +42,73 @@ Args:
 """
 function GaussianFilter(θmaj::Real, θmin::Real=-1, ϕ::Real=0; θunit=rad, ϕunit=deg)
     return create_filter(Gaussian, θmaj * fwhm2σ, θmin * fwhm2σ, ϕ, θunit=θunit, ϕunit=ϕunit)
+end
+
+"""
+    EllipticalGaussian(F, θmaj, [θmin, ϕ, x0, y0]; [θunit, ϕunit])
+
+Create an elliptical Gaussian.
+
+Args:
+- `F::Real`:
+    The total flux desnity of the Gaussian.
+- `θmaj::Real`:
+    The major-axis FWHM size of the Gaussian.
+- `θmin::Real`:
+    The minor-axis FWHM size of the Gaussian. If `θmin < 0`, then
+    `θmin = θmax` (i.e. circular Gaussian). Default to -1.
+- `ϕ::Real`:
+    The position angle of the Gausian. Default to 0.
+- `x0, y0::Real`:
+    The centoral position in the unit of θunit. Default to 0.
+- `θunit::Unitful`:
+    The unit for `θmaj`, `θmin`, `x0` and `y0`. Default: `θunit=rad`.
+- `ϕunit::Unitful`:
+    The unit for `ϕ`. Default: `ϕ=deg`.
+"""
+function EllipticalGaussian(F::Real, θmaj::Real, θmin::Real=-1, ϕ::Real=0, x0::Real=0, y0::Real=0; θunit=rad, ϕunit=deg)
+    g = create_filter(Gaussian, θmaj * fwhm2σ, θmin * fwhm2σ, ϕ, θunit=θunit, ϕunit=ϕunit)
+
+    if F != 1.0
+        g = g * F
+    end
+
+    if abs(x0) + abs(y0) != 0.0
+        fθ_rad = unitconv(θunit, rad)
+        g = shifted(g, x0 * fθ_rad, y0 * fθ_rad)
+    end
+
+    return g
+end
+
+
+"""
+    CircularGaussian(F, θmaj, [x0, y0]; [θunit, ϕunit])
+
+Create an circular Gaussian.
+
+Args:
+- `F::Real`:
+    The total flux desnity of the Gaussian.
+- `θfwhm::Real`:
+    The FWHM size of the Gaussian.
+- `x0, y0::Real`:
+    The centoral position in the unit of θunit. Default to 0.
+- `θunit::Unitful`:
+    The unit for `θ`, `x0` and `y0`, respectively. 
+    Default: `θunit=rad`.
+"""
+function CircularGaussian(F::Real, θfwhm::Real, x0::Real=0, y0::Real=0; θunit=rad)
+    g = create_filter(Gaussian, θfwhm * fwhm2σ, -1, 0, θunit=θunit)
+
+    if F != 1.0
+        g = g * F
+    end
+
+    if abs(x0) + abs(y0) != 0.0
+        fθ_rad = unitconv(θunit, rad)
+        g = shifted(g, x0 * fθ_rad, y0 * fθ_rad)
+    end
+
+    return g
 end
